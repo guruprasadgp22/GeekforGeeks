9582dab629b1bef21a2428ff6b5242c1c5f13a77
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-06-21 18:09:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        // your code
+        HashSet<Integer> window = new HashSet<>();
+        for(int i=0;i<arr.length;i++) {
+            if(window.contains(arr[i])) {
+                return true;
+            }
+            window.add(arr[i]);
+            if(window.size() > k) {
+                window.remove(arr[i-k]);
+            }
+        }
+        
+        return false;
+    }
+}
