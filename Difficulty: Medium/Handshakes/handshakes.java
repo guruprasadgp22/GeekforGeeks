fda20a02feea50cec4dd9331864f93b1a295f412
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-06-25 20:06:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Handshakes/handshakes.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int count(int N) {
+        // code here
+        int n = N/2;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++) {
+            dp[i] = 0;
+            
+            for(int j=0;j<i;j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        
+        return dp[n];
+    }
+};
