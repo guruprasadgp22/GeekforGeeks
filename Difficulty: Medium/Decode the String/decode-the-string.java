7b4f2b79b52b781de3be37471926c5eb45be7fba
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-06 20:29:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the String/decode-the-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    static String decodedString(String s) {
+        // code here
+        int k = 0;
+        StringBuilder curr = new StringBuilder();
+        Stack<Integer> num = new Stack<>();
+        Stack<StringBuilder> stack = new Stack<>();
+        
+        for(char ch: s.toCharArray()) {
+            if(Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } else if(ch == '[') {
+                num.add(k);
+                stack.add(curr);
+                curr = new StringBuilder();
+                k = 0;
+            } else if(ch == ']') {
+                StringBuilder decode = stack.pop();
+                int n = num.pop();
+                for(int i=0;i<n;i++) {
+                    decode.append(curr);
+                }
+                
+                curr = decode;
+            } else {
+                curr.append(ch);
+            }
+        }
+        
+        return curr.toString();
+    }
+}
